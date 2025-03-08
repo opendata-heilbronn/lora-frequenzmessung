7fb27bb612fa09b1283f72097b4f5129acac375d
niklas_wunnemann <niklas@wuennemann.eu>
2025-03-08 14:08:18 +0100

    added

--- backend/Share/Misc/StartUp.go
+++ backend/Share/Misc/StartUp.go
@@ -1,6 +1,7 @@
 package Misc
 
 import (
+	"errors"
 	"fmt"
 	"github.com/joho/godotenv"
 	"os"
@@ -11,6 +12,11 @@ func StartUp() {
 	loadEnv()
 }
 func loadEnv() {
+	// if dotenv file is not present
+	if _, err := os.Stat(".env"); errors.Is(err, os.ErrNotExist) {
+		println("No .env file found")
+		return
+	}
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("Error loading .env file")
