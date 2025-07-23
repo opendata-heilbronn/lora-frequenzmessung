6d22f1aef3c9223617b5c1efdb37b63543d858c7
harmoniemand <github@hmnd.de>
2025-07-23 23:00:49 +0200

    wip

--- sensor-pax/src/main.cpp
+++ sensor-pax/src/main.cpp
@@ -2,20 +2,130 @@
 #define LIBPAX_WIFI 0
 #define LIBPAX_BLE 1
 
+#define LoRaWAN_DEBUG_LEVEL 0
+
+#define uS_TO_S_FACTOR 1000000ULL
+#define TIME_TO_SLEEP 60 // 900 Sec are 15 minutes
+
 #include <Arduino.h>
-#include "esp_wifi.h"
 #include "libpax_api.h"
+#include "HT_lCMEN2R13EFC1.h"
+#include "cfhn_logo.h"
+#include "customs.h"
 
 
-#define uS_TO_S_FACTOR 1000000ULL /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP 120          /* Time ESP32 will go to sleep (in seconds) */
+struct count_payload_t count_from_libpax;
+bool new_data_available = false;
+int current_count = 0;
+
+// ****** LORA *******
+
+#include "LoRaWan_APP.h"
+
+/*LoraWan region, select in arduino IDE tools*/
+LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
+
+/*LoraWan Class, Class A and Class C are supported*/
+DeviceClass_t loraWanClass = CLASS_A;
+
+/*the application data transmission duty cycle.  value in [ms].*/
+uint32_t appTxDutyCycle = 15000;
+
+/*OTAA or ABP*/
+bool overTheAirActivation = true;
+
+/*ADR enable*/
+bool loraWanAdr = true;
+
+/* Indicates if the node is sending confirmed or unconfirmed messages */
+bool isTxConfirmed = true;
+
+/* Application port */
+uint8_t appPort = 2;
+/*!
+ * Number of trials to transmit the frame, if the LoRaMAC layer did not
+ * receive an acknowledgment. The MAC performs a datarate adaptation,
+ * according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
+ * to the following table:
+ *
+ * Transmission nb | Data Rate
+ * ----------------|-----------
+ * 1 (first)       | DR
+ * 2               | DR
+ * 3               | max(DR-1,0)
+ * 4               | max(DR-1,0)
+ * 5               | max(DR-2,0)
+ * 6               | max(DR-2,0)
+ * 7               | max(DR-3,0)
+ * 8               | max(DR-3,0)
+ *
+ * Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
+ * the datarate, in case the LoRaMAC layer did not receive an acknowledgment
+ */
+uint8_t confirmedNbTrials = 4;
+
+/* Prepares the payload of the frame */
+static void prepareTxFrame(uint8_t port)
+{
+  // Create a simple JSON-like structure
+  String seperator = ",";
+  String string = sensor_id + seperator + String(current_count);
+
+  // Convert the string to bytes
+  appDataSize = string.length() + 1;
+  string.getBytes(appData, appDataSize);
+}
+
+RTC_DATA_ATTR bool firstrun = true;
+
+void init_lora()
+{
+  Mcu.begin(HELTEC_BOARD, SLOW_CLK_TPYE);
+
+  if (firstrun)
+  {
+    LoRaWAN.displayMcuInit();
+    firstrun = false;
+  }
+}
+
+// ****** PAX *******
+
+void log()
+{
+  new_data_available = true;
+  current_count = count_from_libpax.pax;
+}
+
+// libpax initialization
+void init_libpax()
+{
+  struct libpax_config_t configuration;
+  libpax_default_config(&configuration);
+  configuration.blecounter = 1;
+  configuration.blescantime = 0;
+  configuration.wificounter = 0;
+  configuration.wifi_channel_switch_interval = 50;
+  configuration.wifi_rssi_threshold = -80;
+  configuration.ble_rssi_threshold = -80;
+  libpax_update_config(&configuration);
+
+  // internal processing initialization
+  libpax_counter_init(log, &count_from_libpax, 10, 1);
+  libpax_counter_start();
+}
+
+// ****** DEEPSLEEP *******
+
+#define uS_TO_S_FACTOR 1000000
+#define TIME_TO_SLEEP 5
 
 RTC_DATA_ATTR int bootCount = 0;
 
-count_payload_t* current_count;
-
-wifi_country_t country;
-
+/*
+Method to print the reason by which ESP32
+has been awaken from sleep
+*/
 void print_wakeup_reason()
 {
   esp_sleep_wakeup_cause_t wakeup_reason;
@@ -45,60 +155,78 @@ void print_wakeup_reason()
   }
 }
 
-void pax_callback(void)
-{
-  Serial.println("PAX callback");
-
-  Serial.println("PAX: " + String(current_count->pax));
-//  Serial.println("WiFi: " + String(current_count->wifi_count));
-  Serial.println("BLE: " + String(current_count->ble_count));
-
-  // start deep sleep
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Seconds");
-
-  Serial.println("Going to sleep now");
-  Serial.flush();
-  esp_deep_sleep_start();
-}
-
 void setup()
 {
   Serial.begin(115200);
   delay(1000); // Take some time to open up the Serial Monitor
 
-  // Increment boot number and print it every reboot
   ++bootCount;
   Serial.println("Boot number: " + String(bootCount));
 
   // Print the wakeup reason for ESP32
   print_wakeup_reason();
 
-  libpax_config_t* config = (libpax_config_t*)malloc(sizeof(libpax_config_t));
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Seconds");
 
-  Serial.println("PAX config init");
-  libpax_default_config(config);
+  init_libpax();
 
-  config->wificounter = LIBPAX_WIFI;
-
-  Serial.println("updating pax config");
-  libpax_update_config(config);
-
-  Serial.println("PAX counter init");
-  libpax_counter_init(pax_callback, current_count, 20, 0);
-  
-  delay(1000);
+  init_lora();
 }
-
-bool started = false;
 
 void loop()
 {
-  if (!started)
+  switch (deviceState)
   {
-    Serial.println("Starting PAX counter");
-    libpax_counter_start();
-    started = true;
+  case DEVICE_STATE_INIT:
+  {
+#if (LORAWAN_DEVEUI_AUTO)
+    LoRaWAN.generateDeveuiByChipID();
+#endif
+    LoRaWAN.init(loraWanClass, loraWanRegion);
+    // both set join DR and DR when ADR off
+    LoRaWAN.setDefaultDR(3);
+    break;
   }
-  delay(100);
+  case DEVICE_STATE_JOIN:
+  {
+    LoRaWAN.displayJoining();
+    LoRaWAN.join();
+    break;
+  }
+  case DEVICE_STATE_SEND:
+  {
+    LoRaWAN.displaySending();
+    prepareTxFrame(appPort);
+    LoRaWAN.send();
+    deviceState = DEVICE_STATE_CYCLE;
+
+    Serial.println("Going to sleep now");
+    delay(1000);
+    Serial.flush();
+    esp_deep_sleep_start();
+    Serial.println("This will never be printed");
+    
+    break;
+  }
+  case DEVICE_STATE_CYCLE:
+  {
+    // Schedule next packet transmission
+    txDutyCycleTime = appTxDutyCycle + randr(-APP_TX_DUTYCYCLE_RND, APP_TX_DUTYCYCLE_RND);
+    LoRaWAN.cycle(txDutyCycleTime);
+    deviceState = DEVICE_STATE_SLEEP;
+    break;
+  }
+  case DEVICE_STATE_SLEEP:
+  {
+    LoRaWAN.displayAck();
+    LoRaWAN.sleep(loraWanClass);
+    break;
+  }
+  default:
+  {
+    deviceState = DEVICE_STATE_INIT;
+    break;
+  }
+  }
 }
