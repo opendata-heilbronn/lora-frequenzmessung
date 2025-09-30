172e5744a4f7c7100969189283a150b9c5a6d57c
niklas_wunnemann <niklas@wuennemann.eu>
2025-09-30 19:15:44 +0200

    wip

--- sensor-pax/src/main.cpp
+++ sensor-pax/src/main.cpp
@@ -17,7 +17,18 @@ const double MIN_BATT_VOLTAGE = 3.0; // Voltage for 0%
 #include "lora.h"
 #include "pax.h"
 #include "display.h"
+// Battery voltage thresholds for LiPo battery
+const float BATTERY_MAX_VOLTAGE = 4.2;
+const float BATTERY_MIN_VOLTAGE = 3.3;
+const float BATTERY_NOMINAL = 3.7;
 
+// Voltage divider configuration for Heltec V3
+const float R1 = 390.0;  // 390k ohm
+const float R2 = 100.0;  // 100k ohm
+const float VOLTAGE_DIVIDER_RATIO = (R1 + R2) / R2;  // 4.9
+
+const float ADC_REFERENCE_VOLTAGE = 3.3;
+const int ADC_RESOLUTION = 4096;  // 12-bit ADC
 double getBatteryValue() {
   // Set the resolution of the analog-to-digital converter (ADC) to 12 bits (0-4095):
   analogReadResolution(12);
@@ -33,6 +44,29 @@ double getBatteryValue() {
 
   return analogVolts * 490 / 100;
 }
+float readBatteryVoltage() {
+  int total = 0;
+  const int numReadings = 10;
+
+  for(int i = 0; i < numReadings; i++) {
+    total += analogRead(1);
+    delay(10);
+  }
+  int averageReading = total / numReadings;
+  float adcVoltage = (averageReading * ADC_REFERENCE_VOLTAGE) / ADC_RESOLUTION;
+  float batteryVoltage = adcVoltage * VOLTAGE_DIVIDER_RATIO;
+
+  return batteryVoltage;
+}
+float calculateBatteryPercentage(float voltage) {
+  // Clamp voltage to valid range
+  if (voltage > BATTERY_MAX_VOLTAGE) voltage = BATTERY_MAX_VOLTAGE;
+  if (voltage < BATTERY_MIN_VOLTAGE) voltage = BATTERY_MIN_VOLTAGE;
+
+  float percentage = ((voltage - BATTERY_MIN_VOLTAGE) / (BATTERY_MAX_VOLTAGE - BATTERY_MIN_VOLTAGE)) * 100.0;
+
+  return percentage;
+}
 void setup()
 {
   Serial.begin(115200);
@@ -46,23 +80,24 @@ void setup()
   InitLORA();
 
   //batery settup
-  analogReadResolution(12);
-  pinMode(37, OUTPUT);
-  digitalWrite(37, HIGH);
+  //analogReadResolution(12);
+  //pinMode(37, OUTPUT);
+  //digitalWrite(37, HIGH);
   firstrun = true;
-  double battery_voltage = getBatteryValue();
-  Serial.printf("Battery Voltage: %.2fV", battery_voltage);
+  //double battery_voltage = getBatteryValue();
+  //Serial.printf("Battery Voltage: %.2fV", battery_voltage);
 
 }
 
 void loop()
 {
-  double battery_voltage = getBatteryValue();
-  //Serial.printf("Battery Voltage: %.2fV", battery_voltage);
-
+  //float batteryVoltage = readBatteryVoltage();
+  //float batteryPercentageLinear = calculateBatteryPercentage(batteryVoltage);
+  //Serial.printf("Battery Percentage (Linear): %.1f%%\n", batteryPercentageLinear);
+  float batteryPercentageLinear= 0;
   if (current_count != 0)
   {
-    LoopLORA(current_count,battery_voltage);
+    LoopLORA(current_count,batteryPercentageLinear);
   }
   else{
    // Serial.println("0 ergebniss ");
