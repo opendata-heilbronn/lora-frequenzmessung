45434e4611bfac763d801512ac101bed37375015
niklas_wunnemann <niklas@wuennemann.eu>
2026-02-25 20:18:34 +0100

    updated LoRa duty cycle calculation, fixed battery ADC pin, and adjusted power control logic

--- sensor-pax/src/pax.h
+++ sensor-pax/src/pax.h
@@ -5,7 +5,7 @@
 #include "libpax_api.h"
 
 struct count_payload_t count_from_libpax;
-bool new_data_available = false;
+volatile bool new_data_available = false;
 int current_count = 0;
 
 
@@ -15,8 +15,7 @@ void log()
   current_count = count_from_libpax.pax;
 }
 
-// libpax initialization
-void InitPAX()
+void paxSetup()
 {
   struct libpax_config_t configuration;
   libpax_default_config(&configuration);
@@ -28,10 +27,11 @@ void InitPAX()
   configuration.ble_rssi_threshold = -80;
   libpax_update_config(&configuration);
 
-  // internal processing initialization
   libpax_counter_init(log, &count_from_libpax, 10, 1);
   libpax_counter_start();
 }
 
-void LoopPAX() {
+void paxStop()
+{
+  libpax_counter_stop();
 }
